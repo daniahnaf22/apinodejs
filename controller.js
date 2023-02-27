8dcce7c40cc9a621522c1486dd305e41af00c09a
daniahnaf22 <ahnaffalihdani@gmail.com>
2023-02-27 10:22:55 +0700

    GET, POST, DEL, PUT APi

---
+++ controller.js
@@ -0,0 +1,87 @@
+"use strict";
+
+var response = require("./rest");
+var connection = require("./koneksi");
+
+exports.index = function (req, res) {
+  response.ok("Aplikasi RESTAPI berjalan", res);
+};
+
+//menampilkan data
+exports.tampilkanMhs = function (req, res) {
+  connection.query("SELECT * FROM mahasiswa", function (err, rows, fileds) {
+    if (err) {
+      console.log(err);
+    } else {
+      response.ok(rows, res);
+    }
+  });
+};
+
+//nemapilkan data mhs by id
+exports.tampilkanMhsById = function (req, res) {
+  let id = req.params.id;
+  connection.query(
+    "SELECT * FROM mahasiswa WHERE id_mhs = ?",
+    [id],
+    function (err, rows, fields) {
+      if (err) {
+        console.log(err);
+      } else {
+        response.ok(rows, res);
+      }
+    }
+  );
+};
+
+// menambahkan data mahasiswa
+exports.tambahkanMhs = function (req, res) {
+  var nama = req.body.nama;
+  var nim = req.body.nim;
+  var jurusan = req.body.jurusan;
+
+  connection.query(
+    "INSERT INTO mahasiswa (nama,nim,jurusan) VALUES(?,?,?)",
+    [nama, nim, jurusan],
+    function (err, rows, fields) {
+      if (err) {
+        console.log(err);
+      } else {
+        response.ok("Berhasil menambahkan Data", res);
+      }
+    }
+  );
+};
+
+// mengubah data mahasiswa by id
+exports.ubahMhs = function (req, res) {
+  var id = req.body.id_mhs;
+  var nama = req.body.nama;
+  var nim = req.body.nim;
+  var jurusan = req.body.jurusan;
+
+  connection.query(
+    "UPDATE mahasiswa SET nama=?, nim=?, jurusan=? WHERE id_mhs=?",
+    [nama, nim, jurusan, id],
+    function (err, rows, fields) {
+      if (err) {
+        console.log(err);
+      } else {
+        response.ok("Berhasil Ubah Data", res);
+      }
+    });
+};
+
+
+// menghapus data berdasarkan id
+exports.hapusMhs = function(req, res) {
+    var id = req.body.id_mhs;
+    connection.query("DELETE FROM mahasiswa WHERE id_mhs=?",[id],
+    function (err, rows, fields) {
+        if (err) {
+          console.log(err);
+        } else {
+          response.ok("Berhasil Hapus Data", res);
+        }
+      });
+}
---
+++ routes.js
@@ -0,0 +1,17 @@
+"use strict";
+
+module.exports = function (app) {
+  var jsonku = require("./controller");
+
+  app.route("/").get(jsonku.index);
+
+  app.route("/show").get(jsonku.tampilkanMhs);
+
+  app.route("/show/:id").get(jsonku.tampilkanMhsById);
+
+  app.route("/post").post(jsonku.tambahkanMhs);
+
+  app.route("/update").put(jsonku.ubahMhs);
+
+  app.route("/delete").delete(jsonku.hapusMhs);
+};
